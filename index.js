ce6c8d910e8e38d773843e47c860984db6585448
DarkImposter <psexton@cfsnc.org>
2018-12-18 11:39:02 -0500

    backend 2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const http = require("http")
 const path = require("path")
 const app = express()
-
-
-butthead
+const server = http.Server(app)
 
 app.get("/", (req, res)=> {
    res.sendFile(path.resolve("sireclub.html"))
 
 })
 app.use(express.static("static"))
+port = process.env.PORT || 3000
+server.listen(port, ()=> {
+  console.log(port)
+})
